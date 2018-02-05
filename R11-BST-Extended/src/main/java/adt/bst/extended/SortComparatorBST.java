393e7978416675bbfe8639479ffa41f08e477e35
geovanens <geovane.silva@ccc.ufcg.edu.br>
2018-02-05 10:38:15 -0300

    up

--- R11-BST-Extended/src/main/java/adt/bst/extended/SortComparatorBST.java
+++ R11-BST-Extended/src/main/java/adt/bst/extended/SortComparatorBST.java
@@ -1,5 +1,15 @@
 package adt.bst.extended;
 
-public interface SortComparatorBST {
+import adt.bst.BST;
+
+public interface SortComparatorBST<T extends Comparable<T>> extends BST<T> {
+	
+	public T[] sort(T[] array);
+	
+	/**
+	 * DIR, RAIZ, ESQ
+	 * @return
+	 */
+	public T[] reverseOrde();
 
 }
